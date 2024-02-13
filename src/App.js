aa38a5754efd5d78fb584482d62376de5b7c6706
Zero <jgm262@naver.com>
2024-02-13 16:38:53 +0900

    감정 일기장 CSR(Client Side Rendering)을 위한 Route 및 Link 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import RouteTest from './components/RouteTest';
+
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
+import New from './pages/New';
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/edit' element={<Edit />} />
+          <Route path='/diary' element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
